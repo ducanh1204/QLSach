964d098de6fb16cda74850cce0442e983aed581b
Duc Anh <nguyenducanh12042000@gmail.com>
2019-10-14 12:15:03 +0700

    a

--- app/src/main/java/vn/edu/poly/qlsach/Home/HomeActivity.java
+++ app/src/main/java/vn/edu/poly/qlsach/Home/HomeActivity.java
@@ -25,7 +25,7 @@ public class HomeActivity extends BaseActivity {
 
     @Override
     public void initView() {
-        setTitle("Quản lý Sách");
+        setTitle("Quản lý Sác");
     }
 
     @Override
