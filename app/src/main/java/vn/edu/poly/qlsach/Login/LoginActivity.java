b5205a780e9776293d8b4ad48fd860328da02f38
Duc Anh <nguyenducanh12042000@gmail.com>
2019-10-21 19:34:05 +0700

    a

--- app/src/main/java/vn/edu/poly/qlsach/Login/LoginActivity.java
+++ app/src/main/java/vn/edu/poly/qlsach/Login/LoginActivity.java
@@ -2,6 +2,9 @@ package vn.edu.poly.qlsach.Login;
 
 
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 import vn.edu.poly.qlsach.Home.HomeActivity;
 import vn.edu.poly.qlsach.Home.BaseActivity;
@@ -9,6 +12,9 @@ import vn.edu.poly.qlsach.Home.MySqliteOpenHelper;
 import vn.edu.poly.qlsach.R;
 
 public class LoginActivity extends BaseActivity {
+
+    private TextInputEditText edtuser, edtpass;
+    private String userName, passWord;
 
     @Override
     public int setLayout() {
@@ -21,6 +27,9 @@ public class LoginActivity extends BaseActivity {
     public void initView() {
         mySqliteOpenHelper = new MySqliteOpenHelper(this);
         mySqliteOpenHelper.createDataBase();
+        edtpass = findViewById(R.id.edtpassWord);
+        edtuser = findViewById(R.id.edtuserName);
+
     }
 
     public void openForgotPass(View view) {
@@ -32,6 +41,12 @@ public class LoginActivity extends BaseActivity {
     }
 
     public void openHome(View view) {
-        openActivity(HomeActivity.class);
+        userName = edtuser.getText().toString();
+        passWord = edtpass.getText().toString();
+        if (userName.equals("admin") && passWord.equals("admin")) {
+            openActivity(HomeActivity.class);
+        } else {
+            Toast.makeText(this, "Nhập tài khoản với vai trò người quản lý", Toast.LENGTH_SHORT).show();
+        }
     }
 }
